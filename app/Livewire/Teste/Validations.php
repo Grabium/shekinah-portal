8c697e93be43d5b1d465c7808f5cfab863723e80
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2025-11-10 00:25:48 -0300

    anotações sobre o wire:model='form.name' nas tags

--- app/Livewire/Teste/Validations.php
+++ app/Livewire/Teste/Validations.php
@@ -2,17 +2,42 @@
 
 namespace App\Livewire\Teste;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Validations extends Component
 {
-    public string $name, $email, $msg;
+    #[Validate(['required', 'min:3'])] //valida apenas $name
+    public $name;
+
+    #[Validate(['required', 'min:3', 'email'])] //valida apenas $email
+    public $email;
+
+    /**
+     * as propriedades de formulário (name e email) podem ser encapsuladas em outra classe
+     * faça: $ php artisan livewire:form Client
+     * Um arquivo seria gerado no: App/Livewire/Form/Cliente.php
+     * Já nas tags html, pode-se acessar essas propriedades com wire:model="form.name"
+     * Crie a variável que representa o formulário: public Client $form;
+     * Acesse um array com as propriedades do form: $this->form->all()
+     */
+
+    public $msg;
 
     public function store()
     {
-        //dd($this->name, $this->email);
+
+
+        $this->validate(); //chama o que voi combinado no ANOTATIONS.
+
+        
+
         //Simlua que dados foram persistidos
-        //$this->redirect('/teste-treinaweb/home');
+        /*
+        User::create(['name'=>$this->name,'email'=>$this->email]);
+        $this->redirect('/teste-treinaweb/home');
+        */
+
         $this->msg = 
         'Novo cadastro gerado:'.PHP_EOL.
         'Nome: '.$this->name.PHP_EOL.
--- resources/views/livewire/teste/validations.blade.php
+++ resources/views/livewire/teste/validations.blade.php
@@ -7,11 +7,17 @@
         <div>
             <label for="name">Nome</label>
             <input type="text" wire:model="name" name="name" id="name">
+            @error('name') {{-- Condicional --}}
+                <div>{{$message}}</div> {{-- $message é uma variavel reservada pelo livewire --}}
+            @enderror
         </div>
 
         <div>
             <label for="email">Email</label>
             <input type="email" wire:model="email" name="email" id="email">
+            @error('email')
+                <div>{{$message}}</div> {{-- $message é uma variavel reservada pelo livewire --}}
+            @enderror
         </div>
 
         <button type="submit">Enviar</button>
